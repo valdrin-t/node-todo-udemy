a152f5170bdd27cb4cb106514dd2a9663500e0f4
Valdrin Trena <valdrint00+git@gmail.com>
2018-12-23 00:29:30 +0100

    set up app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
 
+const dbLink = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app'
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {
+mongoose.connect(dbLink,{//'mongodb://127.0.0.1:27017/TodoApp', {
     useNewUrlParser: true,
     useCreateIndex: true,
 });
