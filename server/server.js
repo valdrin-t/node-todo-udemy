587353687ec5fb8a81eeb6bae6154b3a294eb32c
Valdrin Trena <valdrint00+git@gmail.com>
2018-12-22 20:43:38 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,15 @@ app.post('/todos', (req, res) => {
         // console.log('Something went wrong. ', e);
         res.status(400).send(e);
     });
-})
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) =>{
+        res.status(400).send(e);
+    });
+});
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
