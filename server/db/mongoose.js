6afe677e3714ba1022b86974abe089f331613715
Valdrin Trena <valdrint00+git@gmail.com>
2018-12-22 21:29:16 +0100

    Add tests for GET /todos

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', { useNewUrlParser: true });
+mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
 
-module.exports = {mongoose};
+module.exports = { mongoose };
