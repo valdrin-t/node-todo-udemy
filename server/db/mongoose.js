9918963f4e8868106179b34d0abed8482fa8e801
Valdrin Trena <valdrint00+git@gmail.com>
2019-01-02 23:14:20 +0100

    put dblink to heroku link for upload

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
-// const dbLink = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app';
-const dbLink = 'mongodb://127.0.0.1:27017/TodoApp';
+const dbLink = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app';
+// const dbLink = 'mongodb://127.0.0.1:27017/TodoApp';
 mongoose.Promise = global.Promise;
 mongoose.connect(dbLink, {
     useNewUrlParser: true,
