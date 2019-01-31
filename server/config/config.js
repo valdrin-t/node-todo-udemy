64f67823e91f8b29821d944a2bf067dc01375550
Valdrin Trena <valdrint00+git@gmail.com>
2019-01-31 12:59:45 +0100

    Removed Commented code in config.js

--- server/config/config.js
+++ server/config/config.js
@@ -7,12 +7,3 @@ if (env === 'development' || env === 'test') {
         process.env[key] = envConfig[key];
     });
 }
-// if (env === 'development') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-// } else if (env === 'test') {
-//     process.env.PORT = 3000;
-//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-// } else if (env === 'production') {
-//     process.env.MONGODB_URI = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app'
-// }
