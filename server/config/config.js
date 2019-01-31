32ec2347c883645d12d05b35ac638b45a7c8893d
Valdrin Trena <valdrint00+git@gmail.com>
2019-01-31 12:51:09 +0100

    Update Config to use JSON file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,18 @@
 var env = process.env.NODE_ENV || 'development';
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
 
-if (env === 'development') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
-} else if (env === 'test') {
-    process.env.PORT = 3000;
-    process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-} else if (env === 'production') {
-    process.env.MONGODB_URI = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app'
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
 }
+// if (env === 'development') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoApp';
+// } else if (env === 'test') {
+//     process.env.PORT = 3000;
+//     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
+// } else if (env === 'production') {
+//     process.env.MONGODB_URI = 'mongodb://trena99:valdrin123@ds241664.mlab.com:41664/todo-app'
+// }
